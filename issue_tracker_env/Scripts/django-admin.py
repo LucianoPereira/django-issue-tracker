e1bbc69a2c440842c7fecfd4e53af33bc29b3dea
Luciano Pereira <lucianopereira9555@gmail.com>
2021-06-12 14:47:01 -0300

    fix .gitignore

--- issue_tracker_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\54221\documents\projects\issue_tracker_2\issue_tracker_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
